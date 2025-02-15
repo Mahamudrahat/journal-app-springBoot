16e7e395c40f4f24cf20b1d57af68eb8cefd0063
hasanrahat <rahat.ruet@gmail.com>
2025-02-16 01:09:18 +0600

    Response Entity Add

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -4,13 +4,12 @@ import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.servcice.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
@@ -26,18 +25,26 @@ public class JournalEntryController {
     }
 
     @PostMapping
-    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+        try{
+            myEntry.setDateTime(LocalDateTime.now());
+            journalEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry,HttpStatus.CREATED) ;
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST) ;
+        }
 //        journalEntries.put(myEntry.getId(), myEntry);
-        myEntry.setDateTime(LocalDateTime.now());
-        journalEntryService.saveEntry(myEntry);
-        return myEntry;
+
 
     }
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId) {
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
 
-        return journalEntryService.findById(myId).orElse(null);
-
+        Optional<JournalEntry> journalEntry=journalEntryService.findById(myId);
+        if(journalEntry.isPresent()){
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
